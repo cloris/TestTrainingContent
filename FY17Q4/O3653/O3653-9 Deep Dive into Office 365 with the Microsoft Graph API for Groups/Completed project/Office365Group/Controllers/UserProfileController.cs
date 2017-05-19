f95bb52fde4ef1e3d6f2a81513f4b6e55a2dc7ff
cloris <suheart@sohu.com>
2017-05-19 19:11:44 +0800

    3-9

---
+++ FY17Q4/O3653/O3653-9 Deep Dive into Office 365 with the Microsoft Graph API for Groups/Completed project/Office365Group/Controllers/UserProfileController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using Microsoft.Azure.ActiveDirectory.GraphClient;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Office365Group.Models;
+
+namespace Office365Group.Controllers
+{
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private string appKey = ConfigurationManager.AppSettings["ida:ClientSecret"];
+        private string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private string graphResourceID = "https://graph.windows.net";
+
+        // GET: UserProfile
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                var graphServiceClient = await GroupRespository.GetGraphServiceAsync();
+                var user = await graphServiceClient.Me.Request().GetAsync();
+                return View(user);
+            }
+            catch (AdalException ex)
+            {
+                // Return to error page.
+                return View("Error");
+            }
+            // if the above failed, the user needs to explicitly re-authenticate for the app to obtain the required token
+            catch (Exception)
+            {
+                return View("Relogin");
+            }
+        }
+
+        public void RefreshSession()
+        {
+            HttpContext.GetOwinContext().Authentication.Challenge(
+                new AuthenticationProperties { RedirectUri = "/UserProfile" },
+                OpenIdConnectAuthenticationDefaults.AuthenticationType);
+        }
+    }
+}
